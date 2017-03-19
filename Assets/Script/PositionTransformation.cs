a3227876abdb12475a4666f538901adb47f0589c
HuangZhuo <idorable@163.com>
2017-03-19 09:29:40 +0800

    initial commit

---
+++ Assets/Script/PositionTransformation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+class PositionTransformation : Transformation
+{
+    public Vector3 position;
+
+    public override Vector3 Apply(Vector3 point)
+    {
+        return point + position;
+    }
+
+    public override Matrix4x4 Matrix
+    {
+        get
+        {
+            var matrix = new Matrix4x4();
+            matrix.SetRow(0, new Vector4(1, 0, 0, position.x));
+            matrix.SetRow(1, new Vector4(0, 1, 0, position.y));
+            matrix.SetRow(2, new Vector4(0, 0, 1, position.z));
+            matrix.SetRow(3, new Vector4(0, 0, 0, 1));
+            return matrix;
+        }
+    }
+}
---
+++ Assets/Script/Transformation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// 变换基类
+public abstract class Transformation : MonoBehaviour {
+
+    // 第一版：直接做变换
+    public abstract Vector3 Apply(Vector3 point);
+
+    // 使用变换矩阵
+    public abstract Matrix4x4 Matrix { get; }
+
+}
