08e470da39c9723285707e8c5d6ad6d1a14a09d7
HuangZhuo <idorable@163.com>
2017-03-19 11:33:46 +0800

    save scene

--- Assets/Script/CameraTransformation.cs
+++ Assets/Script/CameraTransformation.cs
@@ -39,3 +39,6 @@ class CameraTransformation : Transformation
         }
     }
 }
+
+// 概念
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
