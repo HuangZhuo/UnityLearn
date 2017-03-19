f6c5a43b15ccc0df7a6c31127421aa8681c9b2d3
HuangZhuo <idorable@163.com>
2017-03-19 10:56:35 +0800

    camera transformation

--- Assets/Script/CameraTransformation.cs
+++ Assets/Script/CameraTransformation.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 
 class CameraTransformation : Transformation
 {
+    // 焦距
+    public float focalLength;
+
     public override Vector3 Apply(Vector3 point)
     {
         throw new NotImplementedException();
@@ -13,13 +16,25 @@ class CameraTransformation : Transformation
 
     public override Matrix4x4 Matrix
     {
+        // 平行投影到XY平面
+        //get
+        //{
+        //    var matrix = new Matrix4x4();
+        //    matrix.SetRow(0, new Vector4(1, 0, 0, 0));
+        //    matrix.SetRow(1, new Vector4(0, 1, 0, 0));
+        //    matrix.SetRow(2, new Vector4(0, 0, 0, 0));
+        //    matrix.SetRow(3, new Vector4(0, 0, 0, 1));
+        //    return matrix;
+        //}
+
+        // 透视投影
         get
         {
             var matrix = new Matrix4x4();
-            matrix.SetRow(0, new Vector4(1, 0, 0, 0));
-            matrix.SetRow(1, new Vector4(0, 1, 0, 0));
+            matrix.SetRow(0, new Vector4(focalLength, 0, 0, 0));
+            matrix.SetRow(1, new Vector4(0, focalLength, 0, 0));
             matrix.SetRow(2, new Vector4(0, 0, 0, 0));
-            matrix.SetRow(3, new Vector4(0, 0, 0, 1));
+            matrix.SetRow(3, new Vector4(0, 0, 1, 0));
             return matrix;
         }
     }
--- Assets/Script/PositionTransformation.cs
+++ Assets/Script/PositionTransformation.cs
@@ -20,7 +20,7 @@ class PositionTransformation : Transformation
             matrix.SetRow(1, new Vector4(0, 1, 0, position.y));
             matrix.SetRow(2, new Vector4(0, 0, 1, position.z));
             matrix.SetRow(3, new Vector4(0, 0, 0, 1));
-            return matrix;
+            return matrix;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
         }
     }
 }
