dcaa559e8a04c92e69745fadaeffc6da94f98035
HuangZhuo <idorable@163.com>
2017-03-19 10:00:59 +0800

    transformation not initialized

---
+++ Assets/Script/CameraTransformation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Script
+{
+    class CameraTransformation:Transformation
+    {
+        public override Vector3 Apply(Vector3 point)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Matrix4x4 Matrix
+        {
+            get
+            {
+                var matrix = new Matrix4x4();
+                matrix.SetRow(0, new Vector4(1, 0, 0, 0));
+                matrix.SetRow(1, new Vector4(0, 1, 0, 0));
+                matrix.SetRow(2, new Vector4(0, 0, 0, 0));
+                matrix.SetRow(3, new Vector4(0, 0, 0, 1));
+                return matrix;
+            }
+        }
+    }
+}
