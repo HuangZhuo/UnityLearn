9ffa979b72b63e177e611a34e113c9232f7337fb
HuangZhuo <idorable@163.com>
2017-03-19 10:08:38 +0800

    remove default namespace

--- Assets/Script/CameraTransformation.cs
+++ Assets/Script/CameraTransformation.cs
@@ -4,26 +4,23 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
-namespace Assets.Script
+class CameraTransformation : Transformation
 {
-    class CameraTransformation:Transformation
+    public override Vector3 Apply(Vector3 point)
     {
-        public override Vector3 Apply(Vector3 point)
-        {
-            throw new NotImplementedException();
-        }
+        throw new NotImplementedException();
+    }
 
-        public override Matrix4x4 Matrix
+    public override Matrix4x4 Matrix
+    {
+        get
         {
-            get
-            {
-                var matrix = new Matrix4x4();
-                matrix.SetRow(0, new Vector4(1, 0, 0, 0));
-                matrix.SetRow(1, new Vector4(0, 1, 0, 0));
-                matrix.SetRow(2, new Vector4(0, 0, 0, 0));
-                matrix.SetRow(3, new Vector4(0, 0, 0, 1));
-                return matrix;
-            }
+            var matrix = new Matrix4x4();
+            matrix.SetRow(0, new Vector4(1, 0, 0, 0));
+            matrix.SetRow(1, new Vector4(0, 1, 0, 0));
+            matrix.SetRow(2, new Vector4(0, 0, 0, 0));
+            matrix.SetRow(3, new Vector4(0, 0, 0, 1));
+            return matrix;
         }
     }
 }
